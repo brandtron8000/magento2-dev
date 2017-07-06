071ded08165b368b8879da082a91a72776f4366f
Brandon Gonzales <brandtron8k@gmail.com>
2017-07-06 02:03:19 -0700

    Unit 1.7.2.3

---
+++ app/code/Training/Test/Plugin/Magento/Theme/Block/Html/Breadcrumbs/Plugin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Training\Test\Plugin\Magento\Theme\Block\Html\Breadcrumbs;
+
+class Plugin
+{
+    public function beforeaddCrumb(\Magento\Theme\Block\Html\Breadcrumbs $subject, $crumbName, $crumbInfo)
+    {
+      if(is_string($crumbInfo['label']))
+          $crumbInfo['label'] = $crumbInfo['label'].'(\m/)';
+          
+      return [$crumbName,$crumbInfo];
+    }
+}
