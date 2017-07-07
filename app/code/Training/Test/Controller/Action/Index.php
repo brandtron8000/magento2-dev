d9c437ec24d3be610b373e2119df13d97bb43f5f
Brandon Gonzales <brandtron8k@gmail.com>
2017-07-06 21:10:50 -0700

    Unit 2.5.4

--- app/code/Training/Test/Controller/Action/Index.php
+++ app/code/Training/Test/Controller/Action/Index.php
@@ -5,9 +5,11 @@
 * See COPYING.txt for license details.
 */
 namespace Training\Test\Controller\Action;
+
 class Index extends \Magento\Framework\App\Action\Action
 {
   public function execute() {
+    $this->_redirect('catalog/category/view/id/4');
     $helloooo = "<img src='https://media.tenor.com/images/44dbcd137f211757d0685e7d41d694aa/tenor.gif'>";
     $this->getResponse()->appendBody($helloooo);
   }
