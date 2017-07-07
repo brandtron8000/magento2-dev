6bd2cf46c5242eec58696afe76583d76917354af
Brandon Gonzales <brandtron8k@gmail.com>
2017-07-07 00:48:59 -0700

    Unit 3.6.2 Whatever

--- app/code/Training/Test/Controller/Action/Index.php
+++ app/code/Training/Test/Controller/Action/Index.php
@@ -4,13 +4,14 @@
 * Copyright © 2015 Magento. All rights reserved.
 * See COPYING.txt for license details.
 */
+
 namespace Training\Test\Controller\Action;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
   public function execute() {
-    // Disable Redirect to Category ID 4 $this->_redirect('catalog/category/view/id/4');
-    $helloooo = "<img src='https://media.tenor.com/images/44dbcd137f211757d0685e7d41d694aa/tenor.gif'>";
-    $this->getResponse()->appendBody($helloooo);
+    $block = $this->_view->getLayout()->createBlock('Training\Test\Block\Template');
+    $block->setTemplate('Training_Test::test.phtml');
+    $this->getResponse()->appendBody($block->toHtml());
   }
 }
